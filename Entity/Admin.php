2db66c9bf13d720c3bd683f0e2a3b5882d65f533
oughlane <mohammedoughlane72@gmail.com>
2026-01-05 11:40:57 +0100

    ajouter views

---
+++ Entity/Admin.php
@@ -0,0 +1,9 @@
+<?php
+
+class Admin extends Utilisateur {
+
+
+     public function __construct(string $nom,string $prenom,string $email,string $password, bool $actif){
+        parent::__construct($nom,$prenom, $email, 'admin', "",$actif);
+    }
+}
