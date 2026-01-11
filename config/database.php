63d54d0783af9655db3bc83e931cfb98ec6a2b26
oughlane <mohammedoughlane72@gmail.com>
2026-01-11 12:22:27 +0100

    separation de logique et config par crrer une fie reiturn et database class avec getinstance

--- config/database.php
+++ config/database.php
@@ -1,18 +1,13 @@
 <?php
 
+
 return [
-  
-
-    'dsn' => 'mysql:host=localhost;dbname=delivery_app;charset=utf8mb4',
-
+    'dsn' => 'mysql:host=localhost;dbname=delivry_db;charset=utf8mb4',
     'user' => 'root',
-
     'password' => '',
-
-  
     'options' => [
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_CLASS,
-      
-    ],
+        PDO::ATTR_EMULATE_PREPARES => false
+    ]
 ];
